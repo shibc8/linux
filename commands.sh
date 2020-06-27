42b09196bce344ff07e6e2cb1b140d51e8a8face
Shibish <c.shibish@gmail.com>
2020-06-27 09:46:20 +0800

    ZIP and Unzip with TAR added

--- commands.sh
+++ commands.sh
@@ -14,3 +14,9 @@ alt + .
 history
 find
 
+#Install a package
+sudo apt-get install xz-utils
+#Unzip files
+tar -xf node-v12.18.1-linux-x64.tar.xz 
+#Create tar files
+tar -cJf linux-3.12.6.tar.xz linux-3.12.6/
