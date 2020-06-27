b5c3c6b202807d5371eae05fd35dae8917bc7d95
Shibish <c.shibish@gmail.com>
2020-06-27 09:45:48 +0800

    GIT commands added

---
+++ gitCommands.sh
@@ -0,0 +1,11 @@
+git remote add origin https://github.com/shibc8/linux.git
+git push -u origin master
+
+
+
+git checkout branchName
+git checkout -b newBranchName
+
+
+git add .
+git commit -m "Commit Mesage"
