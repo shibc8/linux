10f22caa884bd31df22ca77f0941d5a666ec9105
Shibish <c.shibish@gmail.com>
2021-06-02 14:17:33 +0800

    Removed duplicates

--- commands.sh
+++ commands.sh
@@ -1,26 +1,3 @@
-
-cd 
-
-pwd
-
-echo
-
-exec 
-
-date
-
-# Displays the time taken to complete the command
-## time cmd
-time ls -lrt
-
-# Assign value to any environment variable
-set options
-
-# Exits from the current shell
-exit
-# Exit from the shell that was created during login
-logout
-
 pwd
 ls -lrt
 grep
